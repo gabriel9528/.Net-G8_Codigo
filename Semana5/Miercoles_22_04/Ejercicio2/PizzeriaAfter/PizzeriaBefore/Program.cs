a0393877186ad0e2f206e261553175dc696bd219
Gabriel <gabrielsxf@gmail.com>
2026-04-22 21:00:40 -0500

    semana 5a

---
+++ Semana5/Miercoles_22_04/Ejercicio2/PizzeriaAfter/PizzeriaBefore/Program.cs
@@ -0,0 +1,185 @@
+﻿using static System.Console;
+
+namespace PizzeriaBefore
+{
+    #region Producto abstracto
+    public abstract class Pizza
+    {
+        public string Nombre { get; set; }
+        protected string Masa { get; set; }
+        protected string Salsas { get; set; }
+        protected List<string> Extras { get; set; } = new List<string>();
+
+        public void Prepare()
+        {
+            WriteLine($"Preparando: {Nombre}");
+            WriteLine($"Colocando masa: {Masa}");
+            WriteLine($"Agregando salsas: {Salsas}");
+            WriteLine($"Agregando capas: {string.Join(",", Extras)}");
+        }
+
+        public void Cocinar() => WriteLine($"Cocinar por 20 min");
+        public void Cortar() => WriteLine($"Pizza fue cortada en partes iguales");
+        public void Encajar() => WriteLine($"Pizza colocada en la caja oficial");
+    }
+
+    #endregion
+
+    #region Productos concretos
+
+        #region Arequipa Pizza
+
+        internal class ArequipaPeperoniPizza : Pizza
+        {
+            public ArequipaPeperoniPizza()
+            {
+                Nombre = "Arequipa Peperoni";
+                Masa = "Delgada";
+                Salsas = "Salsa de Tomate";
+                Extras.Add("Salsa de Peperoni");
+            }
+        }
+
+        internal class ArequipaNapolitanaPizza : Pizza
+        {
+            public ArequipaNapolitanaPizza()
+            {
+                Nombre = "Arequipa Napolitana";
+                Masa = "Delgada";
+                Salsas = "Salsa de Tomate";
+                Extras.Add("Salsa de Naploitana");
+                Extras.Add("Extra de queso mozarela");
+            }
+        }
+
+        internal class ArequipaVegetarianaPizza : Pizza
+        {
+            public ArequipaVegetarianaPizza()
+            {
+                Nombre = "Arequipa Vegetariana";
+                Masa = "Delgada";
+                Salsas = "Marinada";
+                Extras.Add("Aceite de oliva");
+                Extras.Add("Salsa de ajo");
+            }
+        }
+
+        #endregion
+
+        #region Lima Pizza
+
+        internal class LimaPeperoniPizza : Pizza
+        {
+            public LimaPeperoniPizza()
+            {
+                Nombre = "Lima Peperoni";
+                Masa = "Delgada";
+                Salsas = "Salsa de Tomate";
+                Extras.Add("Salsa de Peperoni");
+            }
+        }
+
+        internal class LimaNapolitanaPizza : Pizza
+        {
+            public LimaNapolitanaPizza()
+            {
+                Nombre = "Lima Napolitana";
+                Masa = "Delgada";
+                Salsas = "Salsa de Tomate";
+                Extras.Add("Salsa de Naploitana");
+                Extras.Add("Extra de queso mozarela");
+            }
+        }
+
+        internal class LimaVegetarianaPizza : Pizza
+        {
+            public LimaVegetarianaPizza()
+            {
+                Nombre = "Lima Vegetariana";
+                Masa = "Delgada";
+                Salsas = "Marinada";
+                Extras.Add("Aceite de oliva");
+                Extras.Add("Salsa de ajo");
+            }
+        }
+
+    #endregion
+
+    #endregion
+
+    #region Creador Abstracto
+
+    public abstract class PizzaStoreFactory
+    {
+        public abstract Pizza CreatePizza(string name);
+        public Pizza OrderPizza(string name)
+        {
+            Pizza pizza = CreatePizza(name);
+            
+
+            if (pizza != null)
+            {
+                pizza.Prepare();
+                pizza.Cocinar();
+                pizza.Cortar();
+                pizza.Encajar();
+            }
+
+            return pizza;
+        }
+    }
+
+    #endregion
+
+    #region Creador Concreto
+
+    public class ArequipaPizzaStore : PizzaStoreFactory
+    {
+        public override Pizza CreatePizza(string name)
+        {
+            switch(name)
+            {
+                case "Peperoni":
+                    return new ArequipaPeperoniPizza();
+                case "Napolitana":
+                    return new ArequipaNapolitanaPizza();
+                case "Vegetariana":
+                    return new ArequipaVegetarianaPizza();
+                default:
+                    return null;
+            }
+        }
+    }
+
+    public class LimaPizzaStore : PizzaStoreFactory
+    {
+        public override Pizza CreatePizza(string name)
+        {
+            switch (name)
+            {
+                case "Peperoni":
+                    return new LimaPeperoniPizza();
+                case "Napolitana":
+                    return new LimaNapolitanaPizza();
+                case "Vegetariana":
+                    return new LimaVegetarianaPizza();
+                default:
+                    return null;
+            }
+        }
+    }
+
+    #endregion
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PizzaStoreFactory arequipaStore = new ArequipaPizzaStore();
+            Pizza pizza = arequipaStore.OrderPizza("Peperoni");
+            Console.WriteLine($"Pizza: {pizza.Nombre} lista para ser entregada");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Semana5/Miercoles_22_04/Ejercicio2/PizzeriaBefore/PizzeriaBefore/Program.cs
@@ -0,0 +1,100 @@
+﻿using static System.Console;
+
+namespace PizzeriaBefore
+{
+    public class Pizza
+    {
+        public string Nombre { get; set; }
+        protected string Masa { get; set; }
+        protected string Salsas { get; set; }
+        protected List<string> Extras { get; set; } = new List<string>();
+
+        public void Prepare()
+        {
+            WriteLine($"Preparando: {Nombre}");
+            WriteLine($"Colocando masa: {Masa}");
+            WriteLine($"Agregando salsas: {Salsas}");
+            WriteLine($"Agregando capas: {string.Join(",", Extras)}");
+        }
+
+        public void Cocinar() => WriteLine($"Cocinar por 20 min");
+        public void Cortar() => WriteLine($"Pizza fue cortada en partes iguales");
+        public void Encajar() => WriteLine($"Pizza colocada en la caja oficial");
+    }
+
+    public class PizzaStore
+    {
+        public Pizza OrderPizza(string type)
+        {
+            Pizza pizza = null;
+            switch (type)
+            {
+                case "Peperoni":
+                    pizza = new PeperoniPizza();
+                    break;
+                case "Napolitana":
+                    pizza = new NapolitanaPizza();
+                    break;
+                case "Vegetariana":
+                    pizza = new VegetarianaPizza();
+                    break;
+            }
+
+            if (pizza != null)
+            {
+                pizza.Prepare();
+                pizza.Cocinar();
+                pizza.Cortar();
+                pizza.Encajar();
+            }
+
+            return pizza;
+        }
+    }
+
+    internal class PeperoniPizza : Pizza
+    {
+        public PeperoniPizza()
+        {
+            Nombre = "Peperoni";
+            Masa = "Delgada";
+            Salsas = "Salsa de Tomate";
+            Extras.Add("Salsa de Peperoni");
+        }
+    }
+
+    internal class NapolitanaPizza : Pizza
+    {
+        public NapolitanaPizza()
+        {
+            Nombre = "Napolitana";
+            Masa = "Delgada";
+            Salsas = "Salsa de Tomate";
+            Extras.Add("Salsa de Naploitana");
+            Extras.Add("Extra de queso mozarela");
+        }
+    }
+
+    internal class VegetarianaPizza : Pizza
+    {
+        public VegetarianaPizza()
+        {
+            Nombre = "Vegetariana";
+            Masa = "Delgada";
+            Salsas = "Marinada";
+            Extras.Add("Aceite de oliva");
+            Extras.Add("Salsa de ajo");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PizzaStore myStore = new PizzaStore();
+            Pizza pizza = myStore.OrderPizza("Peperoni");
+            Console.WriteLine($"Pizza: {pizza.Nombre} lista para ser entregada");
+            Console.ReadLine();
+        }
+    }
+}
