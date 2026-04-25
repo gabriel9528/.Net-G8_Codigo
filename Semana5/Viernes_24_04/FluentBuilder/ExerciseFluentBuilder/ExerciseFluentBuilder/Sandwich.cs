5c1d04f673e1ad71700be7019d6ccaf7a8f6772e
Gabriel <gabrielsxf@gmail.com>
2026-04-24 22:28:20 -0500

    viernes 24_04

---
+++ Semana5/Viernes_24_04/FluentBuilder/ExerciseFluentBuilder/ExerciseFluentBuilder/Sandwich.cs
@@ -0,0 +1,36 @@
+﻿namespace ExerciseFluentBuilder
+{
+    public class Sandwich
+    {
+        public string Pan {  get; set; }
+        public string Verduras { get; set; }
+        public string Proteinas { get; set; }
+        public string Condimentos { get; set; }
+        public string Queso { get; set; }
+
+        public Sandwich()
+        {
+            
+        }
+
+        public Sandwich(string pan, string verduras, string proteinas, string condimentos, string queso)
+        {
+            Pan = pan;
+            Verduras = verduras;
+            Proteinas = proteinas;
+            Condimentos = condimentos;
+            Queso = queso;
+        }
+
+        public override string ToString()
+        {
+            return $"Sandwich : \n" +
+                $"- Pan: {Pan}\n" +
+                $"- Verduras: {Verduras}\n" +
+                $"- Proteinas: {Proteinas}\n" +
+                $"- Condimentos: {Condimentos}\n" +
+                $"- Queso: {Queso}\n";
+        }
+
+    }
+}
---
+++ Semana5/Viernes_24_04/FluentBuilder/ExerciseFluentBuilder/ExerciseFluentBuilder/SandwichBuilder.cs
@@ -0,0 +1,42 @@
+﻿namespace ExerciseFluentBuilder
+{
+    public class SandwichBuilder
+    {
+        protected Sandwich _sandwich = new Sandwich();
+
+        public SandwichBuilder AgregarPan()
+        {
+            _sandwich.Proteinas = "Pan de molde";
+            return this;
+        }
+
+        public SandwichBuilder AgregarVerduras()
+        {
+            _sandwich.Proteinas = "Lechuga, tomate";
+            return this;
+        }
+
+        public SandwichBuilder AgregarProteina()
+        {
+            _sandwich.Proteinas = "Proteina de carne";
+            return this;
+        }
+
+        public SandwichBuilder AgregarCondimentos()
+        {
+            _sandwich.Proteinas = "Mayonesa, Mostaza";
+            return this;
+        }
+
+        public SandwichBuilder AgregarQueso()
+        {
+            _sandwich.Proteinas = "Queso Cheedar";
+            return this;
+        }
+
+        public Sandwich Build()
+        {
+            return _sandwich;
+        }
+    }
+}
