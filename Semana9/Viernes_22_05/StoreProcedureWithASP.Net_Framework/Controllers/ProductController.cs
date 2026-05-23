dd52cab7ec747487be2019876eba069a53e796d7
Gabriel <gabrielsxf@gmail.com>
2026-05-22 23:08:57 -0500

    semana 9 viernes

---
+++ Semana9/Viernes_22_05/StoreProcedureWithASP.Net_Framework/Controllers/ProductController.cs
@@ -0,0 +1,129 @@
+﻿using StoreProcedureWithASP.Net_Framework.DAL;
+using StoreProcedureWithASP.Net_Framework.Models;
+using System;
+using System.Web.Mvc;
+
+namespace StoreProcedureWithASP.Net_Framework.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductDAL productDAL = new ProductDAL();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            var productList = productDAL.GetAllProducts();
+            if(productList.Count > 0)
+            {
+                return View(productList);
+            }
+            return View();
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            try
+            {
+                var product = productDAL.GetProductById(id);
+                if(product != null)
+                {
+                    return View(product);
+                }
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(ProductModel product)
+        {
+            try
+            {
+                bool isInserted = false;
+                if (ModelState.IsValid)
+                {
+                    isInserted = productDAL.InsertProduct(product);
+                    if (isInserted)
+                        return RedirectToAction("Index");
+                    else
+                        return View();
+                }
+                else
+                    return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var product = productDAL.GetProductById(id);
+            if(product != null)
+                return View(product);
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, ProductModel product)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    bool isUpdate = productDAL.UpdateProduct(product);
+                    if (isUpdate)
+                        return RedirectToAction("Index");
+                    return View();
+                }
+
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var product = productDAL.GetProductById(id);
+            if (product != null)
+                return View(product);
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                string result = productDAL.DeleteProduct(id);
+                if (result.Contains("eliminado"))
+                    return RedirectToAction("Index");
+                return View();
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Semana9/Viernes_22_05/StoreProcedureWithASP.Net_Framework/DAL/ProductDAL.cs
+++ Semana9/Viernes_22_05/StoreProcedureWithASP.Net_Framework/DAL/ProductDAL.cs
@@ -1,4 +1,9 @@
-﻿using System.Configuration;
+﻿using StoreProcedureWithASP.Net_Framework.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace StoreProcedureWithASP.Net_Framework.DAL
 {
@@ -6,6 +11,156 @@ namespace StoreProcedureWithASP.Net_Framework.DAL
     {
         string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
 
+        #region GetAllProducts
 
+        public List<ProductModel> GetAllProducts()
+        {
+            List<ProductModel> listProducts = new List<ProductModel> ();
+            using(SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_get_all_products";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dtProducts = new DataTable();
+
+                connection.Open();
+                adapter.Fill(dtProducts);
+                connection.Close();
+
+                foreach(DataRow row in dtProducts.Rows)
+                {
+                    ProductModel product = new ProductModel();
+                    product.id = Convert.ToInt32(row["id"]);
+                    product.product_name = row["product_name"].ToString();
+                    product.price = Convert.ToInt32(row["price"]);
+                    product.quantity = Convert.ToInt32(row["quantity"]);
+                    product.remarks = row["remarks"].ToString();
+
+                    listProducts.Add(product);
+                }
+
+            }
+            return listProducts;
+
+        }
+        #endregion
+
+        #region GetProductById
+
+        public ProductModel GetProductById(int id)
+        {
+            ProductModel product = new ProductModel();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_get_product_by_id";
+                command.Parameters.AddWithValue("@product_id", id);
+
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataTable dtProducts = new DataTable();
+
+                connection.Open();
+                adapter.Fill(dtProducts);
+                connection.Close();
+
+                foreach (DataRow row in dtProducts.Rows)
+                {
+                    product.id = Convert.ToInt32(row["id"]);
+                    product.product_name = row["product_name"].ToString();
+                    product.price = Convert.ToInt32(row["price"]);
+                    product.quantity = Convert.ToInt32(row["quantity"]);
+                    product.remarks = row["remarks"].ToString();
+
+                }
+
+            }
+            return product; ;
+
+        }
+        #endregion
+
+        #region InsertProduct
+
+        public bool InsertProduct(ProductModel product)
+        {
+            int id = 0;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("sp_insert_product", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@product_name", product.product_name);
+                command.Parameters.AddWithValue("@price", product.price);
+                command.Parameters.AddWithValue("@quantity", product.quantity);
+                command.Parameters.AddWithValue("@remarks", product.remarks);
+
+                connection.Open();
+                id = command.ExecuteNonQuery();
+                connection.Close() ;
+
+                if(id > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region UpdateProduct
+
+        public bool UpdateProduct(ProductModel product)
+        {
+            int id = 0;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand("sp_update_product", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@id", product.id);
+                command.Parameters.AddWithValue("@product_name", product.product_name);
+                command.Parameters.AddWithValue("@price", product.price);
+                command.Parameters.AddWithValue("@quantity", product.quantity);
+                command.Parameters.AddWithValue("@remarks", product.remarks);
+
+                connection.Open();
+                id = command.ExecuteNonQuery();
+                connection.Close();
+
+                if (id > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        #endregion
+
+        #region DeleteProduct
+
+        public string DeleteProduct(int id)
+        {
+            string result = string.Empty;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_delete_product";
+                command.Parameters.AddWithValue("@id", id);
+                command.Parameters.Add("@return_message", SqlDbType.VarChar, 255).Direction = ParameterDirection.Output;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                result = command.Parameters["@return_message"].Value.ToString();
+                connection.Close();
+
+            }
+            return result;
+
+        }
+        #endregion
     }
 }
