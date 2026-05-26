50223c8b1e7f4c601b9482949e93e20fe2409917
Gabriel <gabrielsxf@gmail.com>
2026-05-25 20:26:34 -0500

    semana 10 lunes

---
+++ Semana10/Lunes_25_05/Solution1/DapperMVCDemo.Data/DataAccess/ISqlDataAccess.cs
@@ -0,0 +1,11 @@
+﻿namespace DapperMVCDemo.Data.DataAccess
+{
+    public interface ISqlDataAccess
+    {
+        Task<IEnumerable<T>> GetData<T, P>(string spName, P parameters,
+            string connectionId = "DefaultConnection");
+
+        Task SaveData<T>(string spName, T parameters,
+            string connectionId = "DefaultConnection");
+    }
+}
---
+++ Semana10/Lunes_25_05/Solution1/DapperMVCDemo.Data/DataAccess/SqlDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DapperMVCDemo.Data.DataAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<IEnumerable<T>> GetData<T, P>(string spName, P parameters, 
+            string connectionId = "DefaultConnection")
+        {
+            using IDbConnection dbConnection =
+                new SqlConnection(_config.GetConnectionString(connectionId));
+
+            return await dbConnection.QueryAsync<T>(spName, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task SaveData<T>(string spName, T parameters, string connectionId = "DefaultConnection")
+        {
+            using IDbConnection dbConnection =
+                new SqlConnection(_config.GetConnectionString(connectionId));
+
+            await dbConnection.ExecuteAsync(spName, parameters, commandType:CommandType.StoredProcedure);
+        }
+    }
+}
